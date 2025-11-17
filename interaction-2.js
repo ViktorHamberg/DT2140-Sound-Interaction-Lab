62c67847fb7bdac28f0245038b8736e515be1dd6
Viktor Hamberg <vhamb@kth.se>
2025-11-17 17:36:35 +0100

    interaction 2 torpedo 1.1

--- interaction-2.js
+++ interaction-2.js
@@ -110,9 +110,8 @@ function playAudio() {
     if (audioContext.state === 'suspended') {
         return;
     }
-    dspNode.setParamValue("/torpedo/trigger", 0.5)
-    setTimeout(() => { dspNode.setParamValue("/torpedo/trigger", 1) }, 10);
-    setTimeout(() => { dspNode.setParamValue("/torpedo/trigger", 0) }, 10);
+    dspNode.setParamValue("/torpedo/trigger", 1)
+    setTimeout(() => { dspNode.setParamValue("/torpedo/trigger", 0) }, 100);
 }
 
 //==========================================================================================
